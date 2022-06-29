fdade463c1bf1eff8096a6cabee51809ccb3f328
filipasimoe <filipa.barreiro16@gmail.com>
2022-06-29 17:13:10 +0100

    Added nav

---
+++ src/components/Navigation.vue
@@ -0,0 +1,212 @@
+<template>
+  <header :class="{'scrolled-nav' : scrolledNav}">
+    <nav>
+        <div class="branding">
+            <img src="../assets/logo.png" alt="">
+        </div>
+        <ul v-show="!mobile" class="navigation">
+            <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
+            <li><router-link class="link" :to="{name: ''}">Equipa</router-link></li>
+            <li><router-link class="link" :to="{name: ''}">Publicaçõoes</router-link></li>
+            <li><router-link class="link" :to="{name: ''}">Projectos</router-link></li>
+        </ul>
+        <div class="icon">
+            <!-- TODO substituir por ícon font-awesome -->
+            <i @click="toggleMobileNav" v-show="mobile" :class="{ 'icon-active': mobileNav }">O</i>
+        </div>
+        <transition name="mobile-nav">
+            <ul v-show="mobileNav" class="dropdown-nav">
+            <li><router-link class="link" :to="{name: 'Home'}">Home</router-link></li>
+            <li><router-link class="link" :to="{name: ''}">Equipa</router-link></li>
+            <li><router-link class="link" :to="{name: ''}">Publicaçõoes</router-link></li>
+            <li><router-link class="link" :to="{name: ''}">Projectos</router-link></li>
+        </ul>
+        </transition>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+    name: "navigation",
+    data() {
+        return {
+            scrolledNav: null,
+            mobile: true,
+            mobileNav: null,
+            windowWidth: null
+        }
+    },
+    created() {
+        // Listen for when the window is resized
+        window.addEventListener('resize', this.checkScreen);
+        // Explicitly run the method every time the component is created
+        this.checkScreen();
+    },
+    mounted() {
+        window.addEventListener("scroll", this.updateScroll);
+    },
+    methods: {
+        toggleMobileNav() {
+            this.mobileNav = !this.mobileNav;
+        },
+        updateScroll() {
+            const scrollPosition = window.scrollY;
+            if(scrollPosition > 50) {
+                this.scrolledNav = true;
+                return;
+            }
+            this.scrolledNav = false;
+            return;
+        },
+        checkScreen() {
+            this.windowWidth = window.innerWidth;
+            if(this.windowWidth <= 750) {
+                this.mobile = true;
+                return;
+            }
+            this.mobile = false;
+            this.mobileNav = false;
+            return;
+        }
+    }
+};
+</script>
+
+<style scoped>
+header {
+    background-color: hsla(0, 0%, 0%, 0.8);
+    z-index: 99;
+    width: 100%;
+    position: fixed;
+    transition: .5 ease all;
+    color: white;
+}
+
+nav {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    padding: 12px 0;
+    transition: .5 ease all;
+    width: 90%;
+    margin: 0 auto;
+}
+
+ul, .link {
+    font-weight: 500;
+    color: white;
+    list-style: none;
+    text-decoration: none;
+}   
+
+li {
+    text-transform: uppercase;
+    padding: 16px;
+    margin-left: 16px;
+}
+
+.link {
+    font-size: 14px;
+    transition: .5 ease all;
+    padding-bottom: 4px;
+    border-bottom: 1px solid transparent;
+}
+
+.link:hover {
+    color: purple;
+    border-color: red;
+}
+
+.branding {
+    display: flex;
+    align-items: center;
+}
+
+.branding img {
+    width: 50px;
+    transition: .5 ease all;
+}
+
+.navigation {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: flex-end;
+}
+
+.icon {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 24px;
+    height: 100%; 
+}
+
+i { 
+    cursor: pointer;
+    font-size: 24px;
+    transition: .8 ease all;
+}
+
+/* TODO substituir por 90 graus */
+.icon-active {
+    transform: rotate(180deg);
+}
+
+.dropdown-nav {
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    width: 100%;
+    max-width: 250px;
+    height: 100%;
+    background-color: white;
+    top: 0;
+    left: 0;
+}
+
+.dropdown-nav li {
+    margin-left: 0;
+}
+
+.dropdown-nav li .link {
+    color: black;
+}
+
+.scrolled-nav {
+    background-color: black;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1) 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+
+.scrolled-nav nav {
+    pad: 8px 0;
+}
+
+.scrolled-nav nav .branding img {
+    width: 30px;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1) 0 2px 4px -1px rgba(0, 0, 0, 0.06);
+}
+
+.mobile-nav-enter-active {
+    transition: 1s ease all;
+}
+
+.mobile-nav-leave-active {
+    transition: 1s ease all;
+}
+
+
+.mobile-nav-enter-from {
+    transform: translateX(-250px);
+}
+
+.mobile-nav-enter-to {
+    transform: translateX(0);
+}
+
+.mobile-nav-leave-to {
+    transform: translateX(-250px);
+}
+</style>
