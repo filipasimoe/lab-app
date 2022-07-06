e24078cfab725a3769bdb137a13da51e459fae98
Daniela <57196374+danielammartins@users.noreply.github.com>
2022-07-06 23:01:39 +0100

    edit on standby

--- src/views/team/EditPerson.vue
+++ src/views/team/EditPerson.vue
@@ -1,5 +1,8 @@
 <template>
-   wdawwadawdwdwdwdw
+    <form>
+        <input type="text" id="name" placeholder="fdfds">
+    </form>
+   {{ this.person.name }}
 </template>
 
 <script>
@@ -9,23 +12,19 @@ import { useStore } from 'vuex';
 export default {
     name: "EditPerson",
     data() {
-        return {
-            person: ''
-        }
-    },
-    async beforeCreate() {
         const store = useStore();
  
         const id = computed(() => store.state.userId);
 
-        const response = (await fetch(`http://localhost:8000/api/researcher/info/id/${id}`)).json();
-
-        console.log(id)
-        console.log(response)
-
         return {
+            person: '',
             id
         }
+    },
+    async created() {
+        this.person = await (await fetch(`http://localhost:8000/api/researcher/info/id/${this.id}`)).json();
+
+        document.getElementById("name").value = this.person.name
     }
 };
 </script>
